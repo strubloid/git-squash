c7bba88f553f4211743a9f6c9b892c29e9d26d51
Strubloid <strubloid@gmail.com>
2026-08-19 13:58:40 +0100

    adding more things into the main

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
 This is the commit 1
+
+# trying to simulate a merge conflict
+
+This is the main branch with some differences!
+those differences here should be enough for a conflict 
+
+This is the commit 2
